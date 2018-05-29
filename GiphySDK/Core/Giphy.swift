88c9983c70d66202d63a0a8c4ed5557d64749901
Mohamed Aymen Landolsi <mohamed.aymen.landolsi@gmail.com>
2018-05-29 15:52:52 +0200

    Added Pods

---
+++ GiphySDK/Core/Giphy.swift
@@ -0,0 +1,9 @@
+//
+//  Giphy.swift
+//  GiphySDK
+//
+//  Created by Mohamed Aymen Landolsi on 29/05/2018.
+//  Copyright © 2018 Rokridi. All rights reserved.
+//
+
+import Foundation
---
+++ GiphySDK/GiphySDK.h
@@ -0,0 +1,19 @@
+//
+//  GiphySDK.h
+//  GiphySDK
+//
+//  Created by Mohamed Aymen Landolsi on 29/05/2018.
+//  Copyright © 2018 Rokridi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GiphySDK.
+FOUNDATION_EXPORT double GiphySDKVersionNumber;
+
+//! Project version string for GiphySDK.
+FOUNDATION_EXPORT const unsigned char GiphySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GiphySDK/PublicHeader.h>
+
+
